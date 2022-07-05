28f369a8956b02f039f1caf66236cc8b386a1364
NickPak <you@example.com>
2022-07-05 23:27:07 +0800

    delete vcpkg

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,9 @@
 // main.cpp
-#include <sqlite3.h>
 #include <stdio.h>
-#include <fmt/core.h>
 
 // main function!
 int main()
 {
-    fmt::print("Hello, world!\n");
-    printf("%s\n", sqlite3_libversion());
+    printf("Hello, world!\n");
     return 0;
 }
